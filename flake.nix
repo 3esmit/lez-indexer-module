c45df35e75f936e4fc139ef8c725d878426286de
erhant <erhany96@gmail.com>
2026-08-03 12:44:59 +0300

    chore: bump version to 0.2.1

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
-    logos-execution-zone.url = "git+https://github.com/logos-blockchain/logos-execution-zone?ref=main";
+    logos-execution-zone.url = "github:logos-blockchain/logos-execution-zone?ref=v0.2.1";
   };
 
   outputs =
